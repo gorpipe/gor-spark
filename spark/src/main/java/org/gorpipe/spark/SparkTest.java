d132489a97b85866ae0d7345b528312ae4d7a4f7
sigmar <sigmar@wuxinextcode.com>
2020-05-22 09:11:35 +0000

    string util

--- spark/src/main/java/org/gorpipe/spark/SparkTest.java
+++ spark/src/main/java/org/gorpipe/spark/SparkTest.java
@@ -7,6 +7,7 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.MapPartitionsFunction;
 import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.mllib.linalg.Matrices;
 import org.apache.spark.mllib.linalg.Matrix;
@@ -14,6 +15,7 @@ import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.distributed.*;
 import org.apache.spark.sql.*;
+import scala.Tuple2;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -43,17 +45,20 @@ public class SparkTest {
         GorSparkSession gorSparkSession = SparkGOR.createSession(spark);
         JavaSparkContext javaSparkContext = JavaSparkContext.fromSparkContext(spark.sparkContext());
 
+        Dataset<Row> ds = (Dataset<Row>)gorSparkSession.spark("spark <(pgor -split <(gor /gorproject/brca.gor) /gorproject/plink_wes/metadata/AF.gorz | group chrom -count)",null);
+        ds.collectAsList().forEach(System.err::println);
+
         //Dataset<? extends Row> ds = gorSparkSession.spark("spark <(pgor ref/genes.gorz | group chrom -count)",null);
-        Dataset<Row> ds = (Dataset<Row>)gorSparkSession.spark("spark <(partgor -ff <(nor -h /gorproject/plink_wes/buckets.tsv | select 1 | top 1000) -partsize 10 -dict /gorproject/plink_wes/variants.gord <(pgor -split <(gor /gorproject/brca.gor) /gorproject/plink_wes/variants.gord -nf -f #{tags} " +
-                        "| varjoin -r -l -e '?' /gorproject/plink_wes/vep_single.gorz" +
-                        "| where max_consequence in ('frameshift_variant','splice_acceptor_variant','splice_donor_variant','start_lost','stop_gained','stop_lost','incomplete_terminal_codon_variant','inframe_deletion','inframe_insertion','missense_variant','protein_altering_variant','splice_region_variant')))"
-                //"| rename Chrom CHROM | rename ref REF | rename alt ALT " +
+        /*Dataset<Row> ds = (Dataset<Row>)gorSparkSession.spark("spark <(partgor -ff <(nor -h /gorproject/plink_wes/buckets.tsv | select 1 | top 500) -partsize 10 -dict /gorproject/plink_wes/variants.gord <(pgor -split <(gor /gorproject/brca.gor) /gorproject/plink_wes/variants.gord -nf -f #{tags} " +
+                "| varjoin -r -l -e '?' /gorproject/plink_wes/vep_single.gorz" +
+                "| where max_consequence in ('frameshift_variant','splice_acceptor_variant','splice_donor_variant','start_lost','stop_gained','stop_lost','incomplete_terminal_codon_variant','inframe_deletion','inframe_insertion','missense_variant','protein_altering_variant','splice_region_variant')" +
+                "| rename Chrom CHROM | rename ref REF | rename alt ALT " +
                 //"| varjoin -r -l -e '?' /gorproject/plink_wes/vep_single.gorz " +
                 //"| where max_consequence in ('frameshift_variant','splice_acceptor_variant','splice_donor_variant','start_lost','stop_gained','stop_lost','incomplete_terminal_codon_variant','inframe_deletion','inframe_insertion','missense_variant','protein_altering_variant','splice_region_variant') "
                 //"| varjoin -r -l -e 0.0 <(gor /gorproject/plink_wes/metadata/AF.gorz) " +
                 //"| where isfloat(AF) and float(AF) <= 0.05 " +
-                //"| calc ID chrom+'_'+pos+'_'+ref+'_'+alt " +
-                //"| csvsel /gorproject/plink_wes/buckets.tsv <(nor /gorproject/degcan_pns.rep) -u 3 -gc tag,ref,alt -vs 1"
+                "| calc ID chrom+'_'+pos+'_'+ref+'_'+alt " +
+                "| csvsel /gorproject/plink_wes/buckets.tsv <(nor -h /gorproject/plink_wes/buckets.tsv | select 1 | top 500) -u 3 -gc id,ref,alt -vs 1))"
                 , null);
 
         //ds.collectAsList().forEach(System.err::println);
@@ -78,15 +83,31 @@ public class SparkTest {
         Dataset<MatrixEntry> dsm = ds.map((MapFunction<Row, MatrixEntry>)  r -> {
             return new MatrixEntry(0,0,1.0):
         }, menc);
-        CoordinateMatrix cm = new CoordinateMatrix(dsm.rdd());*/
+        CoordinateMatrix cm = new CoordinateMatrix(dsm.rdd());*
 
+        Encoder<BlockMatrix> bmenc = Encoders.kryo(BlockMatrix.class);
         Encoder<Vector> enc = Encoders.kryo(Vector.class);
         /*Dataset<Vector> sparserows = ds.select("values").map((MapFunction<Row, Vector>) value -> {
             String strVector = value.getString(0);
             int[] ii = IntStream.range(0,strVector.length()).filter(i -> strVector.charAt(i)!='0').toArray();
             double[] dd = value.getString(0).chars().filter(c -> c != '0').asDoubleStream().toArray();
             return Vectors.sparse(ii.length, ii, dd);
-        }, enc);*/
+        }, enc);*
+
+        Dataset<BlockMatrix> dbm = ds.select("values").mapPartitions((MapPartitionsFunction<Row, BlockMatrix) input -> {
+            List<double[]> dlist = new ArrayList<>();
+            while(input.hasNext()) {
+                String values = input.next().getString(0);
+                double[] array = values.chars().asDoubleStream().toArray();
+                dlist.add(array);
+            }
+            int rownum = dlist.get(0).length;
+            double[] mat = new double[dlist.size()*rownum];
+            for(int i = 0; i < dlist.size(); i++) System.arraycopy(dlist.get(i), 0, mat, i*rownum, rownum);
+            Matrices.dense(rownum,dlist.size(),mat);
+            //return new BlockMatrix();
+        },bmenc);
+
         Dataset<Vector> denserows = ds.select("values").map((MapFunction<Row, Vector>) value -> Vectors.dense(value.getString(0).chars().asDoubleStream().toArray()), enc);
         JavaRDD<IndexedRow> rddi = denserows.javaRDD().zipWithIndex().map(t -> new IndexedRow(t._2, t._1));
         IndexedRowMatrix irm = new IndexedRowMatrix(rddi.rdd());
@@ -103,8 +124,6 @@ public class SparkTest {
         DenseMatrix dm = projected.toBreeze();
         System.err.println(dm.toString());
                 //.collectAsList().forEach(System.err::println);*/
-
-
     }
 
     public static void test1(String[] args) {
