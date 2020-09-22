b3f4ddac3e124b7d8643b4e2ba8cc130166e5fd8
sigmar <sigmar@wuxinextcode.com>
2020-09-22 10:59:57 +0000

    close processiterator

--- spark/src/main/java/gorsat/process/GorSparkExternalFunction.java
+++ spark/src/main/java/gorsat/process/GorSparkExternalFunction.java
@@ -7,7 +7,9 @@ import org.gorpipe.gor.model.RowBase;
 import org.gorpipe.gor.session.GorSession;
 import org.gorpipe.model.gor.RowObj;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Iterator;
@@ -37,11 +39,33 @@ public class GorSparkExternalFunction implements MapPartitionsFunction<Row, Row>
         ProcessIteratorAdaptor it = new ProcessIteratorAdaptor(gps.getGorContext(), cmd, "", input, null, header, false, Optional.empty(), false,false);
         if(fetchHeader) {
             String rowstr = it.getHeader();
+            it.close();
             int[] sa = RowObj.splitArray(rowstr);
             Row gorrow = new RowBase("chrN", 0, rowstr, sa, null);
             return Collections.singletonList(gorrow).iterator();
         } else {
-            return it;
+            return new Iterator<Row>() {
+                Row last;
+                boolean closed = false;
+
+                @Override
+                public boolean hasNext() {
+                    if(it.hasNext()) {
+                        last = it.next();
+                        return true;
+                    } else if(!closed) {
+                        it.close();
+                        closed = true;
+                    }
+                    return false;
+                }
+
+                @Override
+                public Row next() {
+                    if(last==null) hasNext();
+                    return last;
+                }
+            };
         }
     }
 }
