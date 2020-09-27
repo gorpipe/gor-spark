f596be3ec9ea417c4a460dd7beee33dd6f1c3472
sigmar <sigmar@wuxinextcode.com>
2020-09-27 10:56:17 +0000

    pysparkserver

--- spark/src/main/java/org/gorpipe/spark/redis/GorSparkRedisRunner.java
+++ spark/src/main/java/org/gorpipe/spark/redis/GorSparkRedisRunner.java
@@ -1,10 +1,15 @@
 package org.gorpipe.spark.redis;
 
+import org.apache.spark.api.java.function.VoidFunction2;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.types.*;
 import org.gorpipe.spark.GorSparkSession;
 import org.gorpipe.spark.GorSparkUtilities;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 
 public class GorSparkRedisRunner implements Callable<String> {
@@ -23,8 +28,42 @@ public class GorSparkRedisRunner implements Callable<String> {
 
     @Override
     public String call() throws Exception {
-        StreamingQuery query = sparkSession.readStream().format("redis").option("stream.read.block", 2000).option("stream.keys", "resque").load().writeStream().format("console").start();
-        /*foreachBatch(new VoidFunction2<Dataset<Row>, Long>() {
+        StructField[] fields = {StructField.apply("_id", DataTypes.StringType, true, Metadata.empty()),StructField.apply("status", DataTypes.StringType, true, Metadata.empty())};
+        StructType schema = new StructType(fields);
+        StreamingQuery query = sparkSession.readStream().format("redis")
+                .option("stream.read.batch.size",1)
+                .option("stream.read.block", 1000)
+                .option("stream.keys", "resque")
+                //.option("infer.schema", true)
+                //.option("inferschema", true)
+                .schema(schema)
+                .load().writeStream().outputMode("update")
+                .foreachBatch(new VoidFunction2<Dataset<Row>, Long>() {
+                    @Override
+                    public void call(Dataset<Row> v1, Long v2) throws Exception {
+                        List<Row> rr = v1.collectAsList();
+                        v1.printSchema();
+                        rr.forEach(System.err::println);
+                    }
+                })
+                /*.foreach(new ForeachWriter<Row>() {
+            @Override
+            public boolean open(long partitionId, long epochId) {
+                return true;
+            }
+
+            @Override
+            public void close(Throwable errorOrNull) {
+
+            }
+
+            @Override
+            public void process(Row row) {
+                System.err.println("hey " + row.toString());
+            }
+        })*/
+                .start();
+                /*.foreachBatch(new VoidFunction2<Dataset<Row>, Long>() {
             @Override
             public void call(Dataset<Row> v1, Long v2) throws Exception {
                 v1.foreach(new ForeachFunction<Row>() {
@@ -37,5 +76,14 @@ public class GorSparkRedisRunner implements Callable<String> {
         }).start();*/
         query.awaitTermination();
         return "";
+    }
+
+    public static void main(String[] args) {
+        var grr = new GorSparkRedisRunner();
+        try {
+            grr.call();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
