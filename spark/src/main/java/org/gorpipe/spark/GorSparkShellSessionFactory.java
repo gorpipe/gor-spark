c14d9dbb0cafb1d378820ca52f16fa7d8909095e
sigmar <sigmar@wuxinextcode.com>
2020-11-11 10:29:53 +0000

    to compare

--- spark/src/main/java/org/gorpipe/spark/GorSparkShellSessionFactory.java
+++ spark/src/main/java/org/gorpipe/spark/GorSparkShellSessionFactory.java
@@ -1,10 +1,7 @@
 package org.gorpipe.spark;
 
-import gorsat.process.SessionBasedQueryEvaluator;
-import org.gorpipe.client.FileCache;
-import org.gorpipe.gor.model.DriverBackedFileReader;
+import org.gorpipe.gor.model.GorParallelQueryHandler;
 import org.gorpipe.gor.session.GorSession;
-import org.gorpipe.gor.session.ProjectContext;
 import org.gorpipe.gorshell.GorShellSessionFactory;
 
 import java.util.UUID;
@@ -21,15 +18,7 @@ public class GorSparkShellSessionFactory extends GorShellSessionFactory {
     }
 
     @Override
-    public ProjectContext initProjectContext(FileCache fileCache, GorSession session) {
-        ProjectContext.Builder projectContextBuilder = new ProjectContext.Builder();
-        return projectContextBuilder
-                .setRoot(this.root)
-                .setCacheDir(this.cacheDir)
-                .setConfigFile(this.configFile)
-                .setFileReader(new DriverBackedFileReader("", this.root, null))
-                .setFileCache(fileCache)
-                .setQueryHandler(new GeneralSparkQueryHandler((GorSparkSession)session, null))
-                .setQueryEvaluator(new SessionBasedQueryEvaluator(session)).build();
+    protected GorParallelQueryHandler createQueryHandler(GorSession session) {
+        return new GeneralSparkQueryHandler((GorSparkSession)session, null);
     }
 }
