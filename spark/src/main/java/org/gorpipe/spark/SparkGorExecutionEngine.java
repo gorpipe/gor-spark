6129321a0c47de79155f28650a5e5c38453e0cac
sigmar <sigmar@genuitysci.com>
2022-05-10 16:57:41 +0000

    handle isdir

--- spark/src/main/java/org/gorpipe/spark/SparkGorExecutionEngine.java
+++ spark/src/main/java/org/gorpipe/spark/SparkGorExecutionEngine.java
@@ -91,8 +91,12 @@ public class SparkGorExecutionEngine extends GorExecutionEngine {
             String theHeader = pi.getIterator().getHeader();
             if (outfile != null) {
                 var fileReader = session.getProjectContext().getFileReader();
-                var outwritefile = ((!fileReader.exists(outfile) && outfile.toLowerCase().endsWith(".gord")) || fileReader.isDirectory(outfile)) ? PathUtils.resolve(outfile,pi.isNorContext() ? UUID.randomUUID() +".tsv" : UUID.randomUUID() +".gorz") : outfile;
-                Output ofile = OutFile.apply(outwritefile.toString(), session.getProjectContext().getFileReader(), theHeader, false, false, pi.isNorContext(), true, false, GorIndexType.NONE, Option.<String>empty(), Deflater.BEST_SPEED);
+                var isDir = false;
+                try {
+                    isDir = fileReader.isDirectory(outfile);
+                } catch(Exception ignored) {}
+                var outwritefile = ((!fileReader.exists(outfile) && outfile.toLowerCase().endsWith(".gord")) || isDir) ? PathUtils.resolve(outfile,pi.isNorContext() ? UUID.randomUUID() +".tsv" : UUID.randomUUID() +".gorz") : outfile;
+                Output ofile = OutFile.apply(outwritefile, session.getProjectContext().getFileReader(), theHeader, false, false, pi.isNorContext(), true, false, GorIndexType.NONE, Option.<String>empty(), Deflater.BEST_SPEED);
                 pi.thePipeStep_$eq(pi.thePipeStep().$bar(ofile));
             } else {
                 String header = pi.getHeader();
