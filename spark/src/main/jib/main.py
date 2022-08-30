7a78b0a6cccbe54d1bcd03055d21c1ebabae2cac
sigmar <sigmar@genuitysci.com>
2022-08-30 14:52:24 +0000

    update hadoop

--- spark/src/main/jib/main.py
+++ spark/src/main/jib/main.py
@@ -1,10 +1,13 @@
 import sys
 import os
 import papermill as pm
-ipynbPath = sys.argv[1]
-outputPath = sys.argv[2]
+projectPath = sys.argv[2]
+projectName = sys.argv[4]
+outputPath = sys.argv[6]
+ipynbPath = projectPath + '/' + sys.argv[8]
+outputPath = projectPath + '/' + outputPath + '/' + sys.argv[10]
 params = {}
-for i in range(3, len(sys.argv), 2):
+for i in range(1, len(sys.argv), 2):
     params[sys.argv[i][2:]] = sys.argv[i+1]
 #os.makedirs(outputPath, exist_ok=True)
 pm.execute_notebook(ipynbPath,outputPath,parameters=params,)
